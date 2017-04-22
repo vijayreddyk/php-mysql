25644d0d542ac39927e843c8a8f8b834f9711783
vijayreddyk <kvijay452@gmail.com>
2017-04-22 22:37:10 +0530

    php objects creation and using

--- php_object.php
+++ php_object.php
@@ -1,2 +1,17 @@
 <?php
+class bus{
+	function bus(){
+		$this->wheels = 4;
+		$this->name = "volvo";
+	}
+	function route(){
+		$this->roue = "NH 9";
+		return $this->roue;
+	}
+}
+$newbus = new bus();
+echo $newbus->name."\n";
+echo $newbus->wheels."\n";
+echo $newbus->route()."\n";
+echo $newbus->roue;
 ?>
